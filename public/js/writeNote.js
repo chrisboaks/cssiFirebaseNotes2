16e3f4ebb948b87b4c5211d85704ce1fdd678153
Khiem Nguyen <ppkhiemnguyen@gmail.com>
2021-07-28 18:21:46 +0000

    Add select tag for labels

--- public/js/writeNote.js
+++ public/js/writeNote.js
@@ -16,10 +16,12 @@ const handleNoteSubmit = () => {
   // 1. Capture the form data
   const noteTitle = document.querySelector('#noteTitle');
   const noteText = document.querySelector('#noteText');
+  const noteLabel = document.querySelector('#noteLabel');
   // 2. Format the data and write it to our database
   firebase.database().ref(`users/${googleUser.uid}`).push({
     title: noteTitle.value,
-    text: noteText.value
+    text: noteText.value,
+    label: noteLabel.options[noteLabel.selectedIndex].value,
   })
   // 3. Clear the form so that we can write a new note
   .then(() => {
